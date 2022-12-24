4a2f1bb0a605a68b9040e86458da850701a9d2af
om205 <om030402@gmail.com>
2022-12-25 04:17:18 +0530

    added android support

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ const $pane = document.getElementById('pane')
 
 const ui = new UI()
 
-
 $start.addEventListener('click', event => {
     const snake = new Snake()
     ui.newGame(snake)
+    //dekstop arrow key controls
     document.onkeydown = e => {
-        switch(e.keyCode)
-        {
+        switch (e.keyCode) {
             case 37:
                 snake.turn(4)
                 break
@@ -27,5 +26,42 @@ $start.addEventListener('click', event => {
                 break
         }
     }
-    
+    //mobile swipe controls
+    let xDown = null
+    let yDown = null
+
+    document.addEventListener('touchstart', evt => {
+        const firstTouch = evt.getTouches[0];
+        xDown = firstTouch.clientX;
+        yDown = firstTouch.clientY;
+    }, false)
+
+    document.addEventListener('touchmove', evt => {
+        if (!xDown || !yDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+            if (xDiff > 0) {
+                snake.turn(6)
+            } else {
+                snake.turn(4)
+            }
+        } else {
+            if (yDiff > 0) {
+                snake.turn(2)
+            } else {
+                snake.turn(8)
+            }
+        }
+        xDown = null;
+        yDown = null;
+    }, false);
+
 })
